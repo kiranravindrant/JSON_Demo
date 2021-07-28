5bed2960f05fa88bcc362fc57b6095e31821ec9f
kiranravindrant <kiranravindrant@gmail.com>
2021-07-28 23:53:09 +0530

    [kiran]Added:Json Program files

---
+++ JsonReader/FileReader.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace JsonReader
+{
+    public class FileReader
+    {
+        public List<Inventory> inventories = new List<Inventory>();
+        
+        public List<Inventory> GetInventory()
+        {
+            string text = File.ReadAllText("D:\\New folder\\Data.json");
+            inventories = JsonConvert.DeserializeObject<List<Inventory>>(text);
+            return inventories;
+        }
+
+    }
+}
---
+++ JsonReader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace JsonReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("JSON DEMO");
+            FileReader fileReader = new FileReader();
+            var inventories = fileReader.GetInventory();
+
+            foreach (var i in inventories)
+            {
+                Console.WriteLine("Name : " + i.Name);
+                Console.WriteLine("Weight : " + i.Weight + " kg");
+                Console.WriteLine("Price : " + i.Price + " Rs ");
+                Console.WriteLine("-------");
+                Console.ReadKey();
+
+
+            }
+        }
+    }
+}
